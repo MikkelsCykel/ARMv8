a54be32294f9d7dedc15b83a24865cb9b1606945
Mikkel Rømer <Mikkel@Mikkels-MacBook-Pro-2.local>
2016-06-24 18:33:47 +0200

    1

--- comb/fun.s
+++ comb/fun.s
@@ -49,7 +49,132 @@ fun:
 	.p2align 2
 .L9:
 	subs	w1, w1, #1
-	aese	v0.16b, v1.16b
+	eor     v30.16b, v30.16b, v31.16b
+    eor     v28.16b, v28.16b, v30.16b
+    eor     v26.16b, v26.16b, v29.16b
+    eor     v24.16b, v24.16b, v27.16b
+    eor     v22.16b, v22.16b, v28.16b
+    eor     v0.16b, v1.16b, v0.16b
+    aese    v30.16b, v26.16b
+    aese    v28.16b, v26.16b
+    aese    v26.16b, v26.16b
+    aesmc   v30.16b, v30.16b
+    aese    v24.16b, v26.16b
+    aesmc   v28.16b, v28.16b
+    aese    v22.16b, v26.16b
+    aesmc   v26.16b, v26.16b
+    aese    v0.16b, v26.16b
+    aesmc   v24.16b, v24.16b
+    aese    v30.16b, v26.16b
+    aesmc   v22.16b, v22.16b
+    aese    v28.16b, v26.16b
+    aesmc   v0.16b, v0.16b
+    aese    v26.16b, v26.16b
+    aesmc   v30.16b, v30.16b
+    aese    v24.16b, v26.16b
+    aesmc   v28.16b, v28.16b
+    aese    v22.16b, v26.16b
+    aesmc   v26.16b, v26.16b
+    aese    v0.16b, v26.16b
+    aesmc   v24.16b, v24.16b
+    aese    v30.16b, v26.16b
+    aesmc   v22.16b, v22.16b
+    aese    v28.16b, v26.16b
+    aesmc   v0.16b, v0.16b
+    aese    v26.16b, v26.16b
+    aesmc   v30.16b, v30.16b
+    aese    v24.16b, v26.16b
+    aesmc   v28.16b, v28.16b
+    aese    v22.16b, v26.16b
+    aesmc   v26.16b, v26.16b
+    aese    v0.16b, v26.16b
+    aesmc   v24.16b, v24.16b
+    aese    v30.16b, v26.16b
+    aesmc   v22.16b, v22.16b
+    aese    v28.16b, v26.16b
+    aesmc   v0.16b, v0.16b
+    aese    v26.16b, v26.16b
+    aesmc   v30.16b, v30.16b
+    aese    v24.16b, v26.16b
+    aesmc   v28.16b, v28.16b
+    aese    v22.16b, v26.16b
+    aesmc   v26.16b, v26.16b
+    aese    v0.16b, v26.16b
+    aesmc   v24.16b, v24.16b
+    aese    v30.16b, v26.16b
+    aesmc   v22.16b, v22.16b
+    aese    v28.16b, v26.16b
+    aesmc   v0.16b, v0.16b
+    aese    v26.16b, v26.16b
+    aesmc   v30.16b, v30.16b
+    aese    v24.16b, v26.16b
+    aesmc   v28.16b, v28.16b
+    aese    v22.16b, v26.16b
+    aesmc   v26.16b, v26.16b
+    aese    v0.16b, v26.16b
+    aesmc   v24.16b, v24.16b
+    aese    v30.16b, v26.16b
+    aesmc   v22.16b, v22.16b
+    aese    v28.16b, v26.16b
+    aesmc   v0.16b, v0.16b
+    aese    v26.16b, v26.16b
+    aesmc   v30.16b, v30.16b
+    aese    v24.16b, v26.16b
+    aesmc   v28.16b, v28.16b
+    aese    v22.16b, v26.16b
+    aesmc   v26.16b, v26.16b
+    aese    v0.16b, v26.16b
+    aesmc   v24.16b, v24.16b
+    aese    v30.16b, v26.16b
+    aesmc   v22.16b, v22.16b
+    aese    v28.16b, v26.16b
+    aesmc   v0.16b, v0.16b
+    aese    v26.16b, v26.16b
+    aesmc   v30.16b, v30.16b
+    aese    v24.16b, v26.16b
+    aesmc   v28.16b, v28.16b
+    aese    v22.16b, v26.16b
+    aesmc   v26.16b, v26.16b
+    aese    v0.16b, v26.16b
+    aesmc   v24.16b, v24.16b
+    aese    v30.16b, v26.16b
+    aesmc   v22.16b, v22.16b
+    aese    v28.16b, v26.16b
+    aesmc   v0.16b, v0.16b
+    aese    v26.16b, v26.16b
+    aesmc   v30.16b, v30.16b
+    aese    v24.16b, v26.16b
+    aesmc   v28.16b, v28.16b
+    aese    v22.16b, v26.16b
+    aesmc   v26.16b, v26.16b
+    aese    v0.16b, v26.16b
+    aesmc   v24.16b, v24.16b
+    aese    v30.16b, v26.16b
+    aesmc   v22.16b, v22.16b
+    aese    v28.16b, v26.16b
+    aesmc   v0.16b, v0.16b
+    aese    v26.16b, v26.16b
+    aesmc   v30.16b, v30.16b
+    aese    v24.16b, v26.16b
+    aesmc   v28.16b, v28.16b
+    aese    v22.16b, v26.16b
+    aesmc   v26.16b, v26.16b
+    aese    v0.16b, v26.16b
+    aesmc   v24.16b, v24.16b
+    aese    v30.16b, v26.16b
+    aesmc   v22.16b, v22.16b
+    aese    v28.16b, v26.16b
+    aesmc   v0.16b, v0.16b
+    aese    v26.16b, v26.16b
+    eor     v6.16b, v30.16b, v26.16b
+    aese    v24.16b, v26.16b
+    eor     v5.16b, v28.16b, v26.16b
+    aese    v22.16b, v26.16b
+    eor     v1.16b, v26.16b, v26.16b  
+    aese    v0.16b, v26.16b
+    eor     v16.16b, v24.16b, v26.16b
+    eor     v3.16b, v22.16b, v26.16b
+    eor     v0.16b, v0.16b, v26.16b
 	bne	.L9
 	str	q0, [x0]
 	ret
